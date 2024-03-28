4d06afdd36be6f7461721826fa48783743fc46c6
tkhmrv <semyon.tkhmrv@gmail.com>
2024-03-28 13:59:01 +0300

    format code

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let camera, scene, renderer, world;
 let near, far;
 let pixR = window.devicePixelRatio ? window.devicePixelRatio : 1;
 let cubes = [];
-let sceneOffsetTarget = {x: 0, y: 0};
-let sceneOffset = {x: 0, y: 0};
+let sceneOffsetTarget = { x: 0, y: 0 };
+let sceneOffset = { x: 0, y: 0 };
 
 let today = new Date();
 today.setHours(0);
@@ -22,36 +22,29 @@ let windowManager;
 let initialized = false;
 
 // get time in seconds since beginning of the day (so that all windows use the same time)
-function getTime ()
-{
+function getTime() {
 	return (new Date().getTime() - today) / 1000.0;
 }
 
 
-if (new URLSearchParams(window.location.search).get("clear"))
-{
+if (new URLSearchParams(window.location.search).get("clear")) {
 	localStorage.clear();
 }
-else
-{	
+else {
 	// this code is essential to circumvent that some browsers preload the content of some pages before you actually hit the url
-	document.addEventListener("visibilitychange", () => 
-	{
-		if (document.visibilityState != 'hidden' && !initialized)
-		{
+	document.addEventListener("visibilitychange", () => {
+		if (document.visibilityState != 'hidden' && !initialized) {
 			init();
 		}
 	});
 
 	window.onload = () => {
-		if (document.visibilityState != 'hidden')
-		{
+		if (document.visibilityState != 'hidden') {
 			init();
 		}
 	};
 
-	function init ()
-	{
+	function init() {
 		initialized = true;
 
 		// add a short timeout because window.offsetX reports wrong values before a short period 
@@ -62,39 +55,37 @@ else
 			updateWindowShape(false);
 			render();
 			window.addEventListener('resize', resize);
-		}, 500)	
+		}, 500)
 	}
 
-	function setupScene ()
-	{
+	function setupScene() {
 		camera = new t.OrthographicCamera(0, 0, window.innerWidth, window.innerHeight, -10000, 10000);
-		
+
 		camera.position.z = 2.5;
 		near = camera.position.z - .5;
 		far = camera.position.z + 0.5;
 
 		scene = new t.Scene();
 		scene.background = new t.Color(0.0);
-		scene.add( camera );
+		scene.add(camera);
 
-		renderer = new t.WebGLRenderer({antialias: true, depthBuffer: true});
+		renderer = new t.WebGLRenderer({ antialias: true, depthBuffer: true });
 		renderer.setPixelRatio(pixR);
-	    
-	  	world = new t.Object3D();
+
+		world = new t.Object3D();
 		scene.add(world);
 
 		renderer.domElement.setAttribute("id", "scene");
-		document.body.appendChild( renderer.domElement );
+		document.body.appendChild(renderer.domElement);
 	}
 
-	function setupWindowManager ()
-	{
+	function setupWindowManager() {
 		windowManager = new WindowManager();
 		windowManager.setWinShapeChangeCallback(updateWindowShape);
 		windowManager.setWinChangeCallback(windowsUpdated);
 
 		// here you can add your custom metadata to each windows instance
-		let metaData = {foo: "bar"};
+		let metaData = { foo: "bar" };
 
 		// this will init the windowmanager and add this window to the centralised pool of windows
 		windowManager.init(metaData);
@@ -103,13 +94,11 @@ else
 		windowsUpdated();
 	}
 
-	function windowsUpdated ()
-	{
+	function windowsUpdated() {
 		updateNumberOfCubes();
 	}
 
-	function updateNumberOfCubes ()
-	{
+	function updateNumberOfCubes() {
 		let wins = windowManager.getWindows();
 
 		// remove all cubes
@@ -120,15 +109,14 @@ else
 		cubes = [];
 
 		// add new cubes based on the current window setup
-		for (let i = 0; i < wins.length; i++)
-		{
+		for (let i = 0; i < wins.length; i++) {
 			let win = wins[i];
 
 			let c = new t.Color();
 			c.setHSL(i * .1, 1.0, .5);
 
 			let s = 100 + i * 50;
-			let cube = new t.Mesh(new t.BoxGeometry(s, s, s), new t.MeshBasicMaterial({color: c , wireframe: true}));
+			let cube = new t.Mesh(new t.BoxGeometry(s, s, s), new t.MeshBasicMaterial({ color: c, wireframe: true }));
 			cube.position.x = win.shape.x + (win.shape.w * .5);
 			cube.position.y = win.shape.y + (win.shape.h * .5);
 
@@ -137,16 +125,14 @@ else
 		}
 	}
 
-	function updateWindowShape (easing = true)
-	{
+	function updateWindowShape(easing = true) {
 		// storing the actual offset in a proxy that we update against in the render function
-		sceneOffsetTarget = {x: -window.screenX, y: -window.screenY};
+		sceneOffsetTarget = { x: -window.screenX, y: -window.screenY };
 		if (!easing) sceneOffset = sceneOffsetTarget;
 	}
 
 
-	function render ()
-	{
+	function render() {
 		let t = getTime();
 
 		windowManager.update();
@@ -165,13 +151,12 @@ else
 
 
 		// loop through all our cubes and update their positions based on current window positions
-		for (let i = 0; i < cubes.length; i++)
-		{
+		for (let i = 0; i < cubes.length; i++) {
 			let cube = cubes[i];
 			let win = wins[i];
 			let _t = t;// + i * .2;
 
-			let posTarget = {x: win.shape.x + (win.shape.w * .5), y: win.shape.y + (win.shape.h * .5)}
+			let posTarget = { x: win.shape.x + (win.shape.w * .5), y: win.shape.y + (win.shape.h * .5) }
 
 			cube.position.x = cube.position.x + (posTarget.x - cube.position.x) * falloff;
 			cube.position.y = cube.position.y + (posTarget.y - cube.position.y) * falloff;
@@ -185,13 +170,12 @@ else
 
 
 	// resize the renderer to fit the window size
-	function resize ()
-	{
+	function resize() {
 		let width = window.innerWidth;
 		let height = window.innerHeight
-		
+
 		camera = new t.OrthographicCamera(0, width, 0, height, -10000, 10000);
 		camera.updateProjectionMatrix();
-		renderer.setSize( width, height );
+		renderer.setSize(width, height);
 	}
 }
