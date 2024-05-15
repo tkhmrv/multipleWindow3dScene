1fa1c38ef8f3114f951cc9915d67eaa9b8787bb2
tkhmrv <semyon.tkhmrv@gmail.com>
2024-05-16 00:01:51 +0300

    was done: renaming, comments, simple tweaks

--- WindowManager.js
+++ WindowManager.js
@@ -1,128 +1,137 @@
+// класс обработчика окон, все свойства ниже приватные
 class WindowManager {
-	#windows;
-	#count;
-	#id;
-	#winData;
-	#winShapeChangeCallback;
-	#winChangeCallback;
+	#allWindows; // все окна
+	#countOfWindows; // количество окон
+	#idWindow; // зачем я пишу комментарии? и так понятно, что это - айдишник окна
+	#dataOfWindow; // данные о текущем окне
+	#windowShapeChangeCallback; // свойства для коллбэка изменения формы окна
+	#windowChangeCallback; // свойство для коллбэка изменения количества окон
 
 	constructor() {
-		let that = this;
+		let that = this; // ссылка на объект WindowManager для использования внутри обработчиков событий
 
-		// event listener for when localStorage is changed from another window
+		// обработчик события изменения данных в локльном хранилище из другого окна
 		addEventListener("storage", (event) => {
-			if (event.key == "windows") {
-				let newWindows = JSON.parse(event.newValue);
-				let winChange = that.#didWindowsChange(that.#windows, newWindows);
 
-				that.#windows = newWindows;
+			// проверка на изменения данных в окнах
+			if (event.key == "allWindows") {
+				let newWindows = JSON.parse(event.newValue); // получение обновленных данных об окнах
+				let isWindowsChanged = that.#didWindowsChange(that.#allWindows, newWindows); // проверка изменения окон
 
-				if (winChange) {
-					if (that.#winChangeCallback) that.#winChangeCallback();
+				that.#allWindows = newWindows; // обновляем данные
+
+				// если данные изменились, вызываем коллбэк изменения окон
+				if (isWindowsChanged) {
+					if (that.#windowChangeCallback) that.#windowChangeCallback();
 				}
 			}
 		});
 
-		// event listener for when current window is about to be closed
+		// обработчик события о закрытии текущего окна
 		window.addEventListener('beforeunload', function (e) {
-			let index = that.getWindowIndexFromId(that.#id);
+			let index = that.getWindowIndexFromId(that.#idWindow); // получаем индекс текущего окна в массиве
 
-			//remove this window from the list and update local storage
-			that.#windows.splice(index, 1);
+			// удаляем текущее окно из списка и обновляем данные в localstorage
+			that.#allWindows.splice(index, 1);
 			that.updateWindowsLocalStorage();
 		});
 	}
 
-	// check if theres any changes to the window list
-	#didWindowsChange(pWins, nWins) {
-		if (pWins.length != nWins.length) {
+	// метод для проверки изменений в списке окон
+	#didWindowsChange(previousWindows, newWindows) {
+		if (previousWindows.length != newWindows.length) { // количество поменялось => тру
 			return true;
 		}
 		else {
-			let c = false;
+			let isChanged = false;
 
-			for (let i = 0; i < pWins.length; i++) {
-				if (pWins[i].id != nWins[i].id) c = true;
+			for (let i = 0; i < previousWindows.length; i++) {
+				if (previousWindows[i].idWindow != newWindows[i].idWindow) isChanged = true; // id поменялись => тру
 			}
 
-			return c;
+			return isChanged;
 		}
 	}
 
-	// initiate current window (add metadata for custom data to store with each window instance)
+	// метод инициализации текущего окна
 	init(metaData) {
-		this.#windows = JSON.parse(localStorage.getItem("windows")) || [];
-		this.#count = localStorage.getItem("count") || 0;
-		this.#count++;
+		this.#allWindows = JSON.parse(localStorage.getItem("allWindows")) || []; // получаем данные о всех окнах из localStorage или создаем пустой массив
+		this.#countOfWindows = localStorage.getItem("countOfWindows") || 0; // получаем количество окон или ставим 0
+		this.#countOfWindows++;
 
-		this.#id = this.#count;
-		let shape = this.getWinShape();
-		this.#winData = { id: this.#id, shape: shape, metaData: metaData };
-		this.#windows.push(this.#winData);
+		this.#idWindow = this.#countOfWindows; // ставим id текущего окна
+		let shapeOfWindow = this.getWindowShape(); // получаем размеры
+		this.#dataOfWindow = { idWindow: this.#idWindow, shape: shapeOfWindow, metaData: metaData }; // создаем объект данных о текущем окне
+		this.#allWindows.push(this.#dataOfWindow); // добавляем данные о текущем окне в массив
 
-		localStorage.setItem("count", this.#count);
-		this.updateWindowsLocalStorage();
+		localStorage.setItem("countOfWindows", this.#countOfWindows); // обновляем количество окон в localstorage
+		this.updateWindowsLocalStorage(); // обновляем данные об окнах
 	}
 
-	getWinShape() {
-		let shape = { x: window.screenLeft, y: window.screenTop, w: window.innerWidth, h: window.innerHeight };
-		return shape;
+	// метод обновления данных текущего окна
+	update() {
+		let shapeOfWindow = this.getWindowShape(); // получаем размеры окна
+
+		// проверям на изменение и (не) изменяем
+		if (shapeOfWindow.x != this.#dataOfWindow.shape.x ||
+			shapeOfWindow.y != this.#dataOfWindow.shape.y ||
+			shapeOfWindow.w != this.#dataOfWindow.shape.w ||
+			shapeOfWindow.h != this.#dataOfWindow.shape.h) {
+			this.#dataOfWindow.shape = shapeOfWindow;
+
+			let index = this.getWindowIndexFromId(this.#idWindow); // получаем индекс
+			this.#allWindows[index].shape = shapeOfWindow; // обновляем данные о размерах окна
+
+			if (this.#windowShapeChangeCallback) this.#windowShapeChangeCallback();
+			this.updateWindowsLocalStorage();
+		}
 	}
 
-	getWindowIndexFromId(id) {
+	// метод получения размеров текущего окна
+	getWindowShape() {
+		let shapeOfWindow = { x: window.screenLeft, y: window.screenTop, w: window.innerWidth, h: window.innerHeight };
+		return shapeOfWindow;
+	}
+
+	// метод получения индекса окна по его id
+	getWindowIndexFromId(idWindow) {
 		let index = -1;
 
-		for (let i = 0; i < this.#windows.length; i++) {
-			if (this.#windows[i].id == id) index = i;
+		for (let i = 0; i < this.#allWindows.length; i++) {
+			if (this.#allWindows[i].idWindow == idWindow) index = i;
 		}
 
 		return index;
 	}
 
+	// метод обновления данных об окнах в localstorage
 	updateWindowsLocalStorage() {
-		localStorage.setItem("windows", JSON.stringify(this.#windows));
+		localStorage.setItem("allWindows", JSON.stringify(this.#allWindows));
 	}
 
-	update() {
-		//console.log(step);
-		let winShape = this.getWinShape();
-
-		//console.log(winShape.x, winShape.y);
-
-		if (winShape.x != this.#winData.shape.x ||
-			winShape.y != this.#winData.shape.y ||
-			winShape.w != this.#winData.shape.w ||
-			winShape.h != this.#winData.shape.h) {
-
-			this.#winData.shape = winShape;
-
-			let index = this.getWindowIndexFromId(this.#id);
-			this.#windows[index].shape = winShape;
-
-			//console.log(windows);
-			if (this.#winShapeChangeCallback) this.#winShapeChangeCallback();
-			this.updateWindowsLocalStorage();
-		}
+	// метод установки коллбэка изменения формы окон
+	setShapeOfWindowChangeCallback(callback) {
+		this.#windowShapeChangeCallback = callback;
 	}
 
-	setWinShapeChangeCallback(callback) {
-		this.#winShapeChangeCallback = callback;
+	// метод установки коллбэка изменения окон
+	setWindowChangeCallback(callback) {
+		this.#windowChangeCallback = callback;
 	}
 
-	setWinChangeCallback(callback) {
-		this.#winChangeCallback = callback;
-	}
-
+	// метод получения всех окон
 	getWindows() {
-		return this.#windows;
+		return this.#allWindows;
 	}
 
+	// метод получения данных о текущем окне
 	getThisWindowData() {
-		return this.#winData;
+		return this.#dataOfWindow;
 	}
 
+	// метод получения идентификатора текущего окна
 	getThisWindowID() {
-		return this.#id;
+		return this.#idWindow;
 	}
 }
 
--- main.js
+++ main.js
@@ -1,39 +1,36 @@
 import WindowManager from './WindowManager.js'
 
-
-
+// инициализация переменных для работы с three.js
 const t = THREE;
-let camera, scene, renderer, world;
-let near, far;
-let pixR = window.devicePixelRatio ? window.devicePixelRatio : 1;
-let cubes = [];
-let sceneOffsetTarget = { x: 0, y: 0 };
-let sceneOffset = { x: 0, y: 0 };
+let camera, scene, renderer, world, windowManager;
+//let near, far; // границы отсечения (используются, если не закомментированы)
+let pixelResolution = window.devicePixelRatio ? window.devicePixelRatio : 1; // получение разрешения экрана в пикселях
+let cubes = []; // массив для хранения кубов
+let sceneOffsetTarget = { x: 0, y: 0 }; // смещение сцены пользователем
+let sceneOffsetActual = { x: 0, y: 0 }; // нынешнее смещение сцены
+let isWindowInitialized = false;
 
-let today = new Date();
-today.setHours(0);
-today.setMinutes(0);
-today.setSeconds(0);
-today.setMilliseconds(0);
-today = today.getTime();
+// установка даты и времени, чтобы все окна работали одновременно
+let currentDate = new Date();
+currentDate.setHours(0);
+currentDate.setMinutes(0);
+currentDate.setSeconds(0);
+currentDate.setMilliseconds(0);
+currentDate = currentDate.getTime(); // преобразование в миллисекунды
 
-let internalTime = getTime();
-let windowManager;
-let initialized = false;
-
-// get time in seconds since beginning of the day (so that all windows use the same time)
+// получаем время в секундах с начала дня
 function getTime() {
-	return (new Date().getTime() - today) / 1000.0;
+	return (new Date().getTime() - currentDate) / 1000.0;
 }
 
-
+// Обработка параметра URL - если в url будет clear, то локальное хранилище очищается
 if (new URLSearchParams(window.location.search).get("clear")) {
 	localStorage.clear();
 }
 else {
-	// this code is essential to circumvent that some browsers preload the content of some pages before you actually hit the url
+	// код для того, чтобы избежать предварительной загрузки содержимого страницы некоторыми браузерами до перехода по url
 	document.addEventListener("visibilitychange", () => {
-		if (document.visibilityState != 'hidden' && !initialized) {
+		if (document.visibilityState != 'hidden' && !isWindowInitialized) {
 			init();
 		}
 	});
@@ -44,45 +41,48 @@ else {
 		}
 	};
 
+	// тут инициализируем
 	function init() {
-		initialized = true;
+		isWindowInitialized = true;
 
-		// add a short timeout because window.offsetX reports wrong values before a short period 
+		// тут добавлен небольшой таймаут, потому что иначе window.offsetX ругается на "неправильные значения"
 		setTimeout(() => {
-			setupScene();
-			setupWindowManager();
-			resize();
-			updateWindowShape(false);
-			render();
-			window.addEventListener('resize', resize);
+			setupScene(); // установка сцены
+			setupWindowManager(); // инициализация менеджера окон
+			resize(); // изменение размеров окна
+			updateWindowShape(false); // обновление формы окна
+			render(); // рендеринг сцены
+			window.addEventListener('resize', resize); // обработчик изменения размеров окна
 		}, 500)
 	}
 
+	// тут настраиваем сцену
 	function setupScene() {
-		camera = new t.OrthographicCamera(0, 0, window.innerWidth, window.innerHeight, -10000, 10000);
+		camera = new t.OrthographicCamera(0, 0, window.innerWidth, window.innerHeight, -10000, 10000); // создание ортографической камеры
 
-		camera.position.z = 2.5;
-		near = camera.position.z - .5;
-		far = camera.position.z + 0.5;
+		camera.position.z = 2.5; // позиция камеры
+		//near = camera.position.z - .5; // ближняя граница отсечения
+		//far = camera.position.z + 0.5; // дальняя граница
 
-		scene = new t.Scene();
-		scene.background = new t.Color(0.0);
-		scene.add(camera);
+		scene = new t.Scene(); // создание сцены
+		scene.background = new t.Color(0.0); // установка фона
+		scene.add(camera); // добавления камеры
 
-		renderer = new t.WebGLRenderer({ antialias: true, depthBuffer: true });
-		renderer.setPixelRatio(pixR);
+		renderer = new t.WebGLRenderer({ antialias: true, depthBuffer: true }); // создание рендера
+		renderer.setPixelRatio(pixelResolution); // установка пискельного отношения
 
-		world = new t.Object3D();
-		scene.add(world);
+		world = new t.Object3D(); // создание объекта
+		scene.add(world); // добавления объекта на сцену
 
-		renderer.domElement.setAttribute("id", "scene");
-		document.body.appendChild(renderer.domElement);
+		renderer.domElement.setAttribute("id", "scene"); // установка id для dom элемента
+		document.body.appendChild(renderer.domElement); // добавление рендера в dom дерево
 	}
 
+	// тут настраиваем менеджер окон
 	function setupWindowManager() {
-		windowManager = new WindowManager();
-		windowManager.setWinShapeChangeCallback(updateWindowShape);
-		windowManager.setWinChangeCallback(windowsUpdated);
+		windowManager = new WindowManager(); // создание экземпляра
+		windowManager.setShapeOfWindowChangeCallback(updateWindowShape); // коллбэк обновления формы окна
+		windowManager.setWindowChangeCallback(updateNumberOfCubes); // коллбэк добавления/убавления окон
 
 		// here you can add your custom metadata to each windows instance
 		let metaData = { foo: "bar" };
@@ -90,86 +90,88 @@ else {
 		// this will init the windowmanager and add this window to the centralised pool of windows
 		windowManager.init(metaData);
 
-		// call update windows initially (it will later be called by the win change callback)
-		windowsUpdated();
-	}
-
-	function windowsUpdated() {
+		// call update windows initially (it will later be called by the window change callback)
 		updateNumberOfCubes();
 	}
 
+	// обновление числа кубов
 	function updateNumberOfCubes() {
-		let wins = windowManager.getWindows();
+		let windows = windowManager.getWindows(); // получение списка окон
 
-		// remove all cubes
-		cubes.forEach((c) => {
-			world.remove(c);
+		// удаление всех кубов
+		cubes.forEach((cube) => {
+			world.remove(cube);
 		})
 
 		cubes = [];
 
-		// add new cubes based on the current window setup
-		for (let i = 0; i < wins.length; i++) {
-			let win = wins[i];
+		// создание новых кубов по актуальному количеству окон
+		for (let i = 0; i < windows.length; i++) {
+			let window = windows[i];
 
-			let c = new t.Color();
-			c.setHSL(i * .1, 1.0, .5);
+			let cubeColor = new t.Color(); // цвет куба
+			cubeColor.setHSL(i * .1, 1.0, .5);
 
-			let s = 100 + i * 50;
-			let cube = new t.Mesh(new t.BoxGeometry(s, s, s), new t.MeshBasicMaterial({ color: c, wireframe: true }));
-			cube.position.x = win.shape.x + (win.shape.w * .5);
-			cube.position.y = win.shape.y + (win.shape.h * .5);
+			let cubeSize = 100 + i * 50; // размер куба
+			//let s2 = 100 + i * 70;
+			//let cube = new t.Mesh(new t.BoxGeometry(cubeSize, cubeSize, cubeSize), new t.MeshBasicMaterial({ color: c, wireframe: true }));
+
+			let cubeMaterial = new t.MeshBasicMaterial({ color: cubeColor, wireframe: true });
+			let cubeGeometry = new t.BoxGeometry(cubeSize, cubeSize, cubeSize, 1, 1, 1); // Используем 4 параметра для 4D-геометрии
+			let cube = new t.Mesh(cubeGeometry, cubeMaterial);
+
+			cube.position.x = window.shape.x + (window.shape.w * .5);
+			cube.position.y = window.shape.y + (window.shape.h * .5);
 
 			world.add(cube);
 			cubes.push(cube);
 		}
 	}
 
+	// обновление формы окна
 	function updateWindowShape(easing = true) {
-		// storing the actual offset in a proxy that we update against in the render function
+
+		// сохраняем фактический оффсет окна в прокси, который обновляется в функции рендеринга
 		sceneOffsetTarget = { x: -window.screenX, y: -window.screenY };
-		if (!easing) sceneOffset = sceneOffsetTarget;
+		if (!easing) sceneOffsetActual = sceneOffsetTarget;
 	}
 
-
+	// рендеринг сцены
 	function render() {
-		let t = getTime();
+		let currentDate = getTime();
 
-		windowManager.update();
+		windowManager.update(); // обновление состояния окон
 
+		// расчет новой позиции на основе разницы между текущим и новым смещением с коэффициентом затухания (для создания плавности)
+		let smoothIndex = .05;
+		sceneOffsetActual.x = sceneOffsetActual.x + ((sceneOffsetTarget.x - sceneOffsetActual.x) * smoothIndex);
+		sceneOffsetActual.y = sceneOffsetActual.y + ((sceneOffsetTarget.y - sceneOffsetActual.y) * smoothIndex);
 
-		// calculate the new position based on the delta between current offset and new offset times a falloff value (to create the nice smoothing effect)
-		let falloff = .05;
-		sceneOffset.x = sceneOffset.x + ((sceneOffsetTarget.x - sceneOffset.x) * falloff);
-		sceneOffset.y = sceneOffset.y + ((sceneOffsetTarget.y - sceneOffset.y) * falloff);
+		// установка позиции мира в смещение
+		world.position.x = sceneOffsetActual.x;
+		world.position.y = sceneOffsetActual.y;
 
-		// set the world position to the offset
-		world.position.x = sceneOffset.x;
-		world.position.y = sceneOffset.y;
+		let windows = windowManager.getWindows(); // получение списка окон
 
-		let wins = windowManager.getWindows();
-
-
-		// loop through all our cubes and update their positions based on current window positions
+		// проходим по всем кубам и обновляем их позицию на основе актульной позиции окон
 		for (let i = 0; i < cubes.length; i++) {
 			let cube = cubes[i];
-			let win = wins[i];
-			let _t = t;// + i * .2;
+			let window = windows[i];
+			let _currentDate = currentDate + i * .5;
 
-			let posTarget = { x: win.shape.x + (win.shape.w * .5), y: win.shape.y + (win.shape.h * .5) }
+			let posTarget = { x: window.shape.x + (window.shape.w * .5), y: window.shape.y + (window.shape.h * .5) }
 
-			cube.position.x = cube.position.x + (posTarget.x - cube.position.x) * falloff;
-			cube.position.y = cube.position.y + (posTarget.y - cube.position.y) * falloff;
-			cube.rotation.x = _t * .5;
-			cube.rotation.y = _t * .3;
+			cube.position.x += (posTarget.x - cube.position.x) * smoothIndex;
+			cube.position.y += (posTarget.y - cube.position.y) * smoothIndex;
+			cube.rotation.x = _currentDate * .5;
+			cube.rotation.y = _currentDate * .3;
 		};
 
 		renderer.render(scene, camera);
 		requestAnimationFrame(render);
 	}
 
-
-	// resize the renderer to fit the window size
+	// изменяем рендерер, чтобы он помещался в окно
 	function resize() {
 		let width = window.innerWidth;
 		let height = window.innerHeight
